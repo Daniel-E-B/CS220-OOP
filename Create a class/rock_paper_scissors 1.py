92f33f4845cc16cddc0ee1cac6e468364c29243a
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-08-30 21:13:33 -0700

    Write a simple card game

--- Create a class/rock_paper_scissors 1.py
+++ Create a class/rock_paper_scissors 1.py
@@ -1,52 +1,30 @@
 import random
 
-CHOICES = ['rock', 'paper', 'scissors']
+class Dealer:
+    def __init__(self):
+        self.cards = ['2','3','4','5','6','7','8','9','10','Jack',
+        'Queen','King','Ace']
+        
+        self.winner=0
+        self.winnerCard=0
+        self.players=input("Rule: \nFirst player to get the highest card wins\n\n\nHow many players?\n")
+        print("\n")
 
-class Action:
-    def __init__(self, prompt):
-        '''Create an action instance, either by randomly choosing the action or prompting the user.'''
-
-        # if an action hasn't been named, choose it randomly
-        if prompt is False:
-            self.name = random.choice(CHOICES)
-        else:
-            while True:
-                self.name = raw_input('Choose {}\n'.format(', '.join(CHOICES)))
-                if self.name in CHOICES:
-                    # the user made a valid choice, so we can stop the loop
-                    break
-                else:
-                    # the user picked something else, so make them choose again
-                    print 'Invalid action {}.'.format(self.name)
-
-        # get the position of the choice in the list
-        self.id = CHOICES.index(self.name)
-
-
-    def compete(self, other_action):
-        '''Compete against another action. Print out who won.'''
-
-        if other_action.id == self.id:
-            print 'Tie! Both chose {}!'.format(self.name)
-
-        # each action is beaten by the action after it in the list
-        # modulo makes it wrap around to the beginning of the list
-        elif ((other_action.id + 1) % len(CHOICES)) == self.id:
-            print '{} beats {}! I win!'.format(self.name.capitalize(), other_action.name)
-
-        else:
-            print '{} beats {}! You win!'.format(other_action.name.capitalize(), self.name)
-
-
+    def deal(self):
+        for i in range (0,self.players):
+            card=random.randint(0,12)
+            if self.winnerCard<card:
+                self.winner=i
+                self.winnerCard=card
+            if card!=12:
+                print "Player ",i+1," got a: ",self.cards[card],"\n"
+            else:
+                print "Player ",i+1," got an: ",self.cards[card],"\n"
+        print "Player ",self.winner+1, " won!"
 # this is a standard Python thing: definitions go above, and any code that will actually
 # run should go into the __main__ section. This way, if someone imports the file because
 # they want to use the functions or classes you've defined, it won't start running your game
 # automatically
 if __name__ == '__main__':
-
-        # Create actions for the two players, computer and user
-        computer_action = Action(prompt=False)
-        user_action = Action(prompt=True)
-
-        # Have the actions play against one another
-        computer_action.compete(user_action)
+    dealer = Dealer()
+    dealer.deal()
