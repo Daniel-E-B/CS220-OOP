5c36ab5095b2091596c00d34a4c5da627c745af5
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-09-02 13:58:07 -0700

    Change Game to Survival
    The new game works like this:
    Every time the mouse is clicked, a new random shape is generated.
    
    If the shape collides with another shape, the one with a larger first side length "takes over" the smaller shape and changes its color.
    
    If a shape does not take anybody over for 1000 game ticks, then the shape dies.
    
    press r to restart

--- Create a class/Cpp shape collisions/Cpp shape collisions/main.cpp
+++ Create a class/Cpp shape collisions/Cpp shape collisions/main.cpp
@@ -8,12 +8,64 @@
 
 using namespace std;
 
+//The objective has changed slightly from that of circles.py
+//When a shape collides with another shape, the smaller of the two will assume the color of the larger
+//If 1000 frames pass without a shape "taking over" another shape, then that shape dies
+
+class Bound{
+public:
+	sf::RectangleShape boundShape;
+	Bound(float x, float y, float w, float h){
+		boundShape.setSize(sf::Vector2f(w, h));
+		boundShape.setPosition(x, y);
+	}
+};
 
 class Shape{
+private:
+	vector<sf::Vector2f>shapePoints;//0 and 1 are two points from the first shape, 2 and 3 are from the second shape
+	float distance(float x1, float x2, float y1, float y2){
+		return(sqrt(pow((x1-x2),2)+pow((y1-y2),2)));
+	}
 public:
 	double speed, direction;
-	void update(){
-		shape.move(speed*cos(direction), speed*sin(direction));
+	int interval=0;//number of frames since takeover
+	void update(sf::RenderWindow &window, vector<Shape> &shapes, vector<Bound> &bounds){
+		//bounce checks:
+		for(int i=0; i<bounds.size(); ++i){
+			if(bounds[i].boundShape.getGlobalBounds().intersects(shape.getGlobalBounds()))
+				shape.rotate(rand()%-45-120);//results in some spazzing, but its not too bad...
+		}
+		shape.move(speed*cos(shape.getRotation()), speed*sin(shape.getRotation()));
+		++interval;
+		
+		//collision checks:
+		for(int i=0; i<shapes.size();++i){
+			for(int j=0; j<shapes.size();++j){
+				if(shapes[i].shape.getGlobalBounds().intersects(shapes[j].shape.getGlobalBounds())){
+					//if two shapes intersect, the one with the longer first sidelength wins
+					if(distance(shapes[i].shape.getPoint(0).x, shapes[i].shape.getPoint(1).x,shapes[i].shape.getPoint(0).y, shapes[i].shape.getPoint(0).y)>distance(shapes[j].shape.getPoint(0).x,shapes[j].shape.getPoint(1).x,shapes[j].shape.getPoint(0).y,shapes[j].shape.getPoint(1).y)){
+						//if shape i wins,
+						//you can't convert somebody on your team!!
+						if(shapes[j].shape.getFillColor()!=shapes[i].shape.getFillColor()){
+						shapes[i].interval=0;
+						shapes[j].shape.setFillColor(shapes[i].shape.getFillColor());
+						}
+					}else{
+						//if shape j wins,
+						if(shapes[j].shape.getFillColor()!=shapes[i].shape.getFillColor()){
+						shapes[j].interval=0;
+						shapes[i].shape.setFillColor(shapes[j].shape.getFillColor());
+						}
+					}
+				}
+			}
+			if(shapes[i].interval>=1000){
+				shapes.erase(shapes.begin()+i);
+			}
+		}
+		
+		window.draw(shape);
 	}
 	sf::ConvexShape shape;
 };
@@ -26,8 +78,8 @@ public:
 		shape.setPosition(rand()%1920, rand()%1080);
 		shape.setFillColor(sf::Color(rand()%255, rand()%255, rand()%255, 255));
 		shape.setOutlineColor(sf::Color::Black);
-		speed=0.5;
-		direction=rand()%360;
+		speed=rand()%8+1;
+		shape.setRotation(rand()%360);
 		shape.setPointCount(4);
 		shape.setPoint(0, sf::Vector2f(0,0));
 		shape.setPoint(1, sf::Vector2f(0,rand()%100));
@@ -44,8 +96,8 @@ public:
 		shape.setPosition(rand()%1920, rand()%1080);
 		shape.setFillColor(sf::Color(rand()%255, rand()%255, rand()%255, 255));
 		shape.setOutlineColor(sf::Color::Black);
-		speed=0.5;
-		direction=rand()%360;
+		speed=rand()%8+1;
+		shape.setRotation(rand()%360);
 		float cornerX=rand()%100;
 		float cornerY=rand()%100;
 		shape.setPointCount(4);
@@ -64,8 +116,8 @@ public:
 		shape.setPosition(rand()%1920, rand()%1080);
 		shape.setFillColor(sf::Color(rand()%255, rand()%255, rand()%255, 255));
 		shape.setOutlineColor(sf::Color::Black);
-		speed=0.5;
-		direction=rand()%360;
+		speed=rand()%8+1;
+		shape.setRotation(rand()%360);
 		shape.setOutlineThickness(3);
 		float p1x=rand()%100;
 		float p2x=rand()%100;
@@ -87,8 +139,8 @@ public:
 		shape.setPosition(rand()%1920, rand()%1080);
 		shape.setFillColor(sf::Color(rand()%255, rand()%255, rand()%255, 255));
 		shape.setOutlineColor(sf::Color::Black);
-		speed=0.5;
-		direction=rand()%360;
+		speed=rand()%8+1;
+		shape.setRotation(rand()%360);
 		size=rand()%100;
 		shape.setOutlineThickness(3);
 		shape.setPointCount(4);
@@ -101,11 +153,17 @@ public:
 };
 
 vector<Shape>gameObjects;
+vector<Bound>gameBounds;
 int shapeSelect=0;
 
 int main()
 {
-
+	//bound setup:
+	gameBounds.push_back(Bound(0, 0, 1920, 100));
+	gameBounds.push_back(Bound(-100, 0, 100, 1080));
+	gameBounds.push_back(Bound(0, 1080, 1920, 100));
+	gameBounds.push_back(Bound(1920, 0, 100, 1080));
+	
 	srand(time(NULL));
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Shape Collisions");
 	window.setVerticalSyncEnabled(true);
@@ -116,6 +174,10 @@ int main()
 	{
 		if (event.type == sf::Event::Closed)
 			window.close();
+		
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)){
+			gameObjects.erase(gameObjects.begin(), gameObjects.end());
+		}
 		
 		if(event.type==sf::Event::MouseButtonPressed){
 			if(event.mouseButton.button==sf::Mouse::Left){
@@ -133,19 +195,9 @@ int main()
 		}
 
 	}
-		//collision checks
-		for(int i=0; i<gameObjects.size(); ++i){
-			for(int j=0; j<gameObjects.size(); ++j){
-				if(gameObjects[i].shape.getGlobalBounds().intersects(gameObjects[j].shape.getGlobalBounds())){
-					gameObjects[i].shape.rotate(180);
-					gameObjects[j].shape.rotate(-180);
-				}
-			}
-		}
 	window.clear(sf::Color::White);
 	for(int i=0; i<gameObjects.size(); ++i){
-		window.draw(gameObjects[i].shape);
-		gameObjects[i].update();
+		gameObjects[i].update(window, gameObjects, gameBounds);
 	}
 	window.display();
 	}
