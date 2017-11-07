6b97abf54b2e9e05622fce63c67dc794e6981181
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-11-07 15:30:10 -0800

    make intellij project with graphics2d to use for gario

---
+++ StockJavaGarIO/src/Main.java
@@ -0,0 +1,8 @@
+import java.awt.Graphics2D;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("began");
+    }
+}
---
+++ StockJavaGarIO/src/RemotePlayer.java
@@ -0,0 +1,2 @@
+public class RemotePlayer {
+}
