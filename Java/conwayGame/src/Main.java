1f56069c570ec9db4ddf0089e7b13bf0579770c9
GOAF-Pilot <GOAF-Pilot@users.noreply.github.com>
2017-12-08 09:32:35 -0800

    configure intellij build options

--- Java/conwayGame/src/Main.java
+++ Java/conwayGame/src/Main.java
@@ -1,27 +1,23 @@
 
 import processing.core.*;
 
-public class ExampleApplet extends PApplet {
+public class Main extends PApplet {
 
     public static void main(String args[]) {
-        PApplet.main("ExampleApplet");
+        PApplet.main("Main");
     }
 
     @Override
     public void settings() {
-        // TODO: Customize screen size and so on here
         size(200, 200);
     }
 
     @Override
     public void setup() {
-        // TODO: Your custom drawing and setup on applet start belongs here
-        clear();
     }
 
     @Override
     public void draw() {
-        // TODO: Do your drawing for each frame here
         clear();
         fill(255);
         rect(50, 50, 100, 100);
